cab3bfbd08428c843b92da0dc51ed7d3f9c686ef
Murshid Alam <65403832+rajbdde@users.noreply.github.com>
2023-01-24 12:00:34 +0000

    Added another function named timeConversion(s)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Index{
+class Index {
 	// https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
 	diagonalDifference(arr) {
 		let ltr = 0;
@@ -41,11 +41,11 @@ class Index{
 	// https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
 	staircase(n) {
 		//process.stdout.write("hello: " + n);
-		for(let i = 1; i <= n; i++) {
-			for(let j = i; j < n; j++) {
+		for (let i = 1; i <= n; i++) {
+			for (let j = i; j < n; j++) {
 				process.stdout.write(" ")
 			}
-			for(let k = 1; k <= i; k++) {
+			for (let k = 1; k <= i; k++) {
 				process.stdout.write("#");
 			}
 			console.log("");
@@ -54,13 +54,13 @@ class Index{
 
 	// https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
 	miniMaxSum(arr) {
-    	arr.sort();
+		arr.sort();
 		let max = 0;
 		let min = 0;
-		for(let i = 1; i < arr.length; i++) {
+		for (let i = 1; i < arr.length; i++) {
 			max += arr[i];
 		}
-		for(let i = 0; i < arr.length - 1; i++) {
+		for (let i = 0; i < arr.length - 1; i++) {
 			min += arr[i];
 		}
 		console.log(min, max);
@@ -73,12 +73,45 @@ class Index{
 		});
 		let total = 0;
 		let lenght = candles.length;
-		for(let i = 0; i < lenght; i++) {
-			if(candles[0] == candles[i]) {
+		for (let i = 0; i < lenght; i++) {
+			if (candles[0] == candles[i]) {
 				total++;
 			}
 		}
 		return total;
+	}
+
+	// https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+	timeConversion(s) {
+		let input = s.split('');
+		let size = input.length;
+		let amPm = input[size - 2] + input[size - 1];
+		let output = parseInt(input[0] + input[1]);
+		let finalOutput = [];
+
+		if (amPm == 'PM') {
+			if (output < 12) {
+				output += 12;
+			} else if(output == 12) {
+				output = output;
+			} else {
+				output += 11;
+			}
+		} else {
+			if (output == 12) {
+				output = '00';
+			} else if(output < 10) {
+				output = '0' + output;
+			}
+		}
+		let changed = (output.toString()).split('');
+		input[0] = changed[0];
+		input[1] = changed[1];
+		
+		for (let i = 0; i < size - 2; i++) {
+			finalOutput.push(input[i]);
+		}
+		return finalOutput.join('');
 	}
 
 }
@@ -89,4 +122,5 @@ const main = new Index();
 // main.staircase(6);
 // main.miniMaxSum([1,3,5,7,9]);
 // console.log(main.birthdayCakeCandles([4,3,4,2]));
-console.log(main.bcc([1,3,4,3,4]));
+// console.log(main.bcc([1,3,4,3,4]));
+console.log(main.timeConversion("06:40:03AM"));
